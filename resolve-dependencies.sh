aa1887aadade067fbf5d36c008e0c42319e5c856
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-05 17:21:58 +0200

    ...

--- resolve-dependencies.sh
+++ resolve-dependencies.sh
@@ -1,1 +1,3 @@
-[-d gwt-2.4.0] || wget http://google-web-toolkit.googlecode.com/files/gwt-2.4.0.zip && unzip gwt-2.4.0.zip && rm -f gwt-2.4.0.zip
+[-d depend] || mkdir depend || exit
+cd depend || exit
+[-d gwt-2.4.0] || wget http://google-web-toolkit.googlecode.com/files/gwt-2.4.0.zip && unzip gwt-2.4.0.zip && rm -f gwt-2.4.0.zip || exit
