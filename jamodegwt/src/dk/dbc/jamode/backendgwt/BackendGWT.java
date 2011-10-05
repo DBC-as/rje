39d4e8536a574b8d79f9082c6ac5b05fe980a350
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-05 17:16:59 +0200

    initial commit

---
+++ jamodegwt/src/dk/dbc/jamode/backendgwt/BackendGWT.java
@@ -0,0 +1,8 @@
+package dk.dbc.jamode.backendgwt;
+import com.google.gwt.core.client.EntryPoint;
+public class BackendGWT implements EntryPoint {
+  public native void onModuleLoad() /*-{
+    console.log("HERE");
+    alert("HERE!");
+  }-*/; 
+}
---
+++ resolve-dependencies.sh
@@ -0,0 +1,1 @@
+[-d gwt-2.4.0] || wget http://google-web-toolkit.googlecode.com/files/gwt-2.4.0.zip && unzip gwt-2.4.0.zip && rm -f gwt-2.4.0.zip
