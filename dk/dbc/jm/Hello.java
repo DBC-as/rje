df91b83596d508be79db16f14af194b44173115a
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-06 10:36:26 +0200

    ...

--- dk/dbc/jm/Hello.java
+++ dk/dbc/jm/Hello.java
@@ -1,5 +1,5 @@
-package dk.dbc.jamode;
-public class JamodeFactory {
+package dk.dbc.jm;
+public class Hello {
     public static String hello() {
         return "Testing... hello from Jamode";
     }
--- gwt/src/dk/dbc/jmimpl/JmImpl.java
+++ gwt/src/dk/dbc/jmimpl/JmImpl.java
@@ -1,10 +1,10 @@
-package dk.dbc.jamodegwt;
-import dk.dbc.jamode.*;
+package dk.dbc.jmimpl;
+import dk.dbc.jm.*;
 import com.google.gwt.core.client.EntryPoint;
 
-public class JamodeGWT implements EntryPoint {
+public class JmImpl implements EntryPoint {
   public void onModuleLoad() {
-    log(JamodeFactory.hello());
+    log(Hello.hello());
   }
   public native void log(String s) /*-{
     console.log(s);
