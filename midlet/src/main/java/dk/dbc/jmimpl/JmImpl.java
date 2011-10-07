05550a72338efe1645ec30c2b57719c3b7336852
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-07 12:42:56 +0200

    ...

--- midlet/src/main/java/dk/dbc/jmimpl/JmImpl.java
+++ midlet/src/main/java/dk/dbc/jmimpl/JmImpl.java
@@ -1,11 +1,25 @@
 package dk.dbc.jmimpl;
 import dk.dbc.jm.*;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
+
+class JmScreen extends Canvas {
+    public JmScreen() {
+        super();
+    }
+    public void paint(Graphics g) {
+        g.setColor(0xff0000);
+        g.fillRect(0,0,getWidth(), getHeight());
+    }
+}
 
 public class JmImpl extends MIDlet {
     private Jm jm;
     boolean initialised = false;
+    JmScreen screen;
 
     public void log(String s) {
         System.out.println(s);
@@ -14,8 +28,11 @@ public class JmImpl extends MIDlet {
     public void startApp() throws MIDletStateChangeException {
         if(!initialised) {
             jm = new Jm(this);
+            screen = new JmScreen();
             jm.init();
         }
+        Display.getDisplay(this).setCurrent(screen);
+        screen.setFullScreenMode(true);
         jm.start();
     }
 
