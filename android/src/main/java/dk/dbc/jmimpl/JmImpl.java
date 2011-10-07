86574ceabb4ba86bedaadeca60a5fba40d170bfc
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-07 11:11:09 +0200

    ...

--- android/src/main/java/dk/dbc/jmimpl/JmImpl.java
+++ android/src/main/java/dk/dbc/jmimpl/JmImpl.java
@@ -24,7 +24,6 @@ public class JmImpl extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
     jm = new Jm(this);
     jm.init();
   }
@@ -40,16 +39,5 @@ public class JmImpl extends Activity {
     super.onPause();
     jm.stop();
   }
-
-
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    public void onCreate() {
-    }
-
 }
 
