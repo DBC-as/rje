f08d0728143d1074fc52c66b98fda772784f8a80
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-14 12:12:04 +0200

    refactor + extra data set

--- zoomsearch/opensearch-data.js
+++ zoomsearch/opensearch-data.js
@@ -1,5 +1,4 @@
-(function() {
-    var classes = [
+    data = [
         "dk.dbc.commons.db.IDBConnection",
         "dk.dbc.commons.db.OracleDBPooledConnection",
         "dk.dbc.commons.db.PostgresqlDBConnection",
@@ -94,52 +93,4 @@
         ];
     //classes = ["a.a", "a.b.a", "a.b.b", "b.a", "c.a", "c.b"];
     //classes = ["a.a.a", "a.a.b"];
-    classes = classes.map(function(s) { return s.split('.') });
-
-    function buildTree(arr) {
-        var prefix = arr[0][0];
-        var result = [];
-        var resultArr = [prefix];
-        arr.forEach(function(elem) {
-            if(elem[0] !== prefix) {
-                result.push(resultArr);
-                prefix = elem[0];
-                resultArr = [prefix];
-            }
-            resultArr.push(elem.slice(1));
-        });
-        result.push(resultArr);
-        result = result.map(function(elem) {
-            if(elem.length === 2 && elem[1].length === 0) {
-                elem = {title: elem[0], weight: 1};
-            } else {
-                var t = buildTree(elem.slice(1));
-                t.unshift(elem[0]);
-                elem = t;
-            }
-            return elem;
-        });
-        return result;
-    }
-
-    function objectify(arr) {
-        if(Array.isArray(arr)) {
-           var result = { title: arr[0]};
-           arr.shift();
-           result.children = arr.map(objectify);
-           return result
-        }
-        return arr;
-    }
-
-    function addWeight(elem) {
-        if(!elem.weight) {
-            elem.children.map(addWeight);
-            elem.weight = elem.children.reduce(function(acc, elem) { return acc + elem.weight; }, 0);
-        } 
-    }
-    data = buildTree(classes);
-    data = data.map(objectify);
-    data.forEach(addWeight);
-    data = data[0];
-})();
+    data = data.map(function(s) { return s.split('.') });
