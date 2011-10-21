03904afae5a9150523af3f2b828c92e890b76050
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-10-21 13:41:36 +0200

    simple canvas-oriented sample midlet

---
+++ gwt2me/samplemidlet/src/main/java/dk/dbc/gwtmidlet/GwtSampleMidlet.java
@@ -0,0 +1,30 @@
+package dk.dbc.gwtmidlet;
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Display;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+class Screen extends Canvas {
+    public Screen() {
+    }
+    public void paint(Graphics g) {
+        g.setColor(0xff0000);
+        g.fillRect(0,0,getWidth(), getHeight());
+    }
+}
+
+public class GwtSampleMidlet extends MIDlet {
+
+    public void startApp() throws MIDletStateChangeException {
+        Screen screen = new Screen();
+        Display.getDisplay(this).setCurrent(screen);
+        screen.setFullScreenMode(true);
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+    }
+}
