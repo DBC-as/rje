df7b27f28b4bba97a8bf3a1ab293d5a36b2d4a35
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-11-07 12:31:42 +0100

    getting started

--- gwtbib/src/main/java/dk/dbc/mobib/GwtMain.java
+++ gwtbib/src/main/java/dk/dbc/mobib/GwtMain.java
@@ -2,10 +2,15 @@ package dk.dbc.mobib;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 
 public class GwtMain implements EntryPoint {
 
 	public void onModuleLoad() {
-		Document.get().getBody().setInnerText("Hello from mobile biblitek.dk app skeleton...");
+		Element	div = Document.get().createDivElement();
+		div.setInnerText("Hello from mobile bibliotek.dk app skeleton...");
+		Document.get().getBody().appendChild(div);
+		
+		
 	}
 }
