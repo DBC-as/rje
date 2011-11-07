15f14f8f818ee63ccdcd4372562cb6aa85fe76b0
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-11-07 12:12:14 +0100

    importing style from javascript app

--- gwtbib/src/main/java/dk/dbc/mobib/GwtMain.java
+++ gwtbib/src/main/java/dk/dbc/mobib/GwtMain.java
@@ -1,10 +1,11 @@
 package dk.dbc.mobib;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.Element;
 
 public class GwtMain implements EntryPoint {
-    public void onModuleLoad() {
-        Document.get().getBody().setInnerText("Hello from mobile biblitek.dk app skeleton...");
-    }
+
+	public void onModuleLoad() {
+		Document.get().getBody().setInnerText("Hello from mobile biblitek.dk app skeleton...");
+	}
 }
