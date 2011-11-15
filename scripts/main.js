99856a028da3d89eaf04fcf5e38ce8ce7f201ee0
Rasmus Erik Voel Jensen <cloud9ide@solsort.dk>
2011-11-15 13:46:32 +0000

    experiments with requirejs

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
-define([], function(require, exports) {
+define(['require', 'exports', 'bibdk'], function(require, exports) {
+        var bibdk = require('bibdk');
+        bibdk.search({
+            cql: 'jensen', 
+            start: 0, 
+            count: 6, 
+            callback: function(result) {
+                console.log(result);
+        }});
+    
     console.log('main');
 });
