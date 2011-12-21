0806cae0a4ad4942d1246ba7269c2864bf1ca679
Rasmus Erik Voel Jensen <github@solsort.dk>
2011-12-21 15:14:27 +0100

    modules

--- m/bibapp/scripts/main.js
+++ m/bibapp/scripts/main.js
@@ -1,201 +1,7 @@
-define(['bibdk'], function(bibdk) {
-
-(function() {
-    var util = window.util = {};
-
-    util.initTransitions = function() {
-        $('body').children().css('visibility', 'hidden');
-        $('body').append('<div class="currentVisible">');
-    };
-
-    function noanimate(elem) {
-        elem.css('-webkit-transition', 'none');
-        elem.css('-moz-transition', 'none');
-        elem.css('transition', 'none');
-    }
-    function animate(elem, time) {
-        elem.css('-webkit-transition', time + 'ms');
-        elem.css('-moz-transition', time + 'ms');
-        elem.css('transition', time + 'ms');
-    }
-
-    util.transitionTo = function(elem, transitionType) {
-        window.scrollTo(0,1);
-        var $prev = $('.currentVisible');
-        var $current = $(elem);
-        if($prev[0] === $current[0]) {
-            return;
-        }
-
-        $bottomfiller = $($('#bottomfiller')[0] || $('<div id="bottomfiller">'));
-        var bottomheight = Math.max(0, $(window).height() + 61 - $current.height());
-        $bottomfiller.css('height', bottomheight);
-        $current.append($bottomfiller);
-
-        [$('.prevVisible'),$prev,$current].forEach(noanimate);
-
-
-        $('.prevVisible')
-            .css('visibility', 'hidden')
-            .removeClass('prevVisible');
-
-        $prev
-            .removeClass('currentVisible')
-            .addClass('prevVisible')
-            ;
-
-        $current.addClass('currentVisible')
-            .css('position', 'relative')
-            .css('top', (-$prev.height()/*+$prev.offset()*/) + 'px')
-            .css('visibility', 'visible')
-            ;
-
-        $('body')
-            .prepend($current)
-            .prepend($prev);
-
-        transitionType= transitionType|| 'slidein';
-        var transitions = {
-            slidein: {
-                time: 400,
-                next: { left: $(window).width() },
-                current: { left: 0 },
-                prev: { left: -$(window).width() }
-            },
-            slideout: {
-                time: 400,
-                next: { left: -$(window).width() },
-                current: { left: 0 },
-                prev: { left: $(window).width() }
-            },
-            fadein: {
-                time: 2000,
-                next: { opacity: 0 },
-                current: { opacity: 1 },
-                prev: { opacity: 0 }
-            },
-            template: {
-                time: 1000,
-                next: { },
-                current: { },
-                prev: { }
-            }
-        };
-
-        $current.css(transitions[transitionType].next);
-        setTimeout(function() {
-            animate($prev, transitions[transitionType].time);
-            animate($current, transitions[transitionType].time);
-            $current.css(transitions[transitionType].current);
-            $prev.css(transitions[transitionType].prev);
-            setTimeout(function() {
-                noanimate($prev);
-                noanimate($current);
-                $('body').append($prev);
-                $prev.css('visible', 'hidden');
-                $current.css('top', '0px');
-            }, transitions[transitionType].time);
-        }, 0);
-    };
-
-    util.topmenu = function(args) {
-        var $topmenu = $($('#topmenu')[0] || '<div id="topmenu">');
-        var menuKeys = Object.keys(args.items);
-        
-        Object.keys(args.items).forEach(function(item) {
-            $topmenu.append(
-                $('<span class="menuitem"></span>')
-                    .text(item)
-                    .bind('touch click', args.items[item])
-                );
-
-        });
-        $('body').append($topmenu);
-        $topmenu
-            .css('position', 'fixed')
-            .css('top', 0)
-            .css('left', 0);
-        
-    };
-})();
-
-(function() {
-    var router;
-    if (typeof exports !== 'undefined') {
-        router = exports;
-    } else {
-        router = {};
-    }
-    if(typeof window !== 'undefined') {
-        window.router = router;
-    }
-    
-    function browserBack() {
-        history.back();
-        history.back();
-    }
-
-    router.history = [];
-
-    function back() {
-        router.history.pop();
-        var prev = router.history.pop();
-        open(prev.name, prev.parameter);
-    }
-    function forward() {
-    }
-
-    function open(name, parameter) {
-        router.history.push({
-            name: name,
-            parameter: parameter
-        }); 
-    }
-
-
-
-    function init() {
-        if(history.pushState) {
-            history.pushState('prev', 'prev', '#prev'); 
-            history.pushState('current', 'current', '#current'); 
-            history.pushState('next', 'next', '#next'); 
-        } else {
-            window.location.hash = "#prev";
-            window.location.hash = "#current";
-            window.location.hash = "#next";
-        }
-        history.back();
-    
-        $(window).bind('hashchange', function() {
-            if(window.location.hash === '#prev') {
-                history.forward();
-                setTimeout(function() {
-                    $(window).trigger('backbutton');
-                },100);
-                router.back();
-            };
-            if(window.location.hash === '#next') {
-                history.back();
-                setTimeout(function() {
-                    $(window).trigger('forwardbutton');
-                },100);
-                router.forward();
-            }
-        });
-    }
-
-
-    router.init = init;
-    router.open = open;
-    router.back = back;
-    router.forward = forward;
-    router.browserBack = browserBack;
-    router.dispatch = {};
-})();
-
+define(['router', 'bibdk', 'uitool'], function(router, bibdk, uitool) {
 $(function(){
-    util.initTransitions();
-    util.transitionTo("#frontpage", 'fadein');
+    uitool.initTransitions();
+    uitool.transitionTo("#frontpage", 'fadein');
     $(window).bind('scroll', function() {
         if($(window).scrollTop() === 0) {
             scrollTo(0, 1);
@@ -203,8 +9,8 @@ $(function(){
     });
 
     //router.init(); // breaks on android
-    util.topmenu({items: {
-        "bibliotek.dk": function() { util.transitionTo("#frontpage", 'slideout'); },
+    uitool.topmenu({items: {
+        "bibliotek.dk": function() { uitool.transitionTo("#frontpage", 'slideout'); },
         "søg": function() { alert("søg"); },
         "lånerstatus": function() { alert("lånerstatus"); }
         /*
@@ -233,7 +39,7 @@ $(function(){
     }
 
     function searchResults(material, query) {
-        util.transitionTo("#searchresultpage");
+        uitool.transitionTo("#searchresultpage");
         $("#searchresultquery").text(query);
         $('#numhits').text("");
         $("#searchresults").html("");
