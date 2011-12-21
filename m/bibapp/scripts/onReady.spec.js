f594771248b5640b26ac4df0cc0773360bdc5d4e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-12-22 00:07:44 +0100

    improved testcase, to have actual comparison rather than just skip if not timeout

--- m/bibapp/scripts/onReady.spec.js
+++ m/bibapp/scripts/onReady.spec.js
@@ -6,7 +6,10 @@ define(['onReady'], function(onReady) {
 
     describe('onReady', function() {
         it('runs script when environment is ready', function() {
-            waitsFor(function() { return initialised; }, "didn't run script within 10 sec.", 10000);
+            waitsFor(function() { 
+                    initialised && expect(initialised).toBe(true);
+                    return initialised; 
+                }, "didn't run script within 10 sec.", 10000);
         });
     });
 });
